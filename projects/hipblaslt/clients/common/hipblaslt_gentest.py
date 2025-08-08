fe42d22b60b25720f32dab7075c060154dc36fb3
Parth Kumar <Parth.Kumar@amd.com>
2025-08-08 12:37:27 -0500

    minor fix

--- projects/hipblaslt/clients/common/hipblaslt_gentest.py
+++ projects/hipblaslt/clients/common/hipblaslt_gentest.py
@@ -395,11 +395,10 @@ def instantiate(test):
       skgrid_vector[0] = 0
     test['skgrid_vector'] = skgrid_vector
 
-
     # Any Arguments fields declared as enums (a_type, b_type, etc.)
     enum_args = [decl[0] for decl in param['Arguments']._fields_
                  if decl[1].__module__ == '__main__']
-    array_value_args = ["gsu_vector", "wgm_vector"]
+    array_value_args = ["gsu_vector", "wgm_vector", "skgrid_vector"]
 
     try:
         setdefaults(test)
